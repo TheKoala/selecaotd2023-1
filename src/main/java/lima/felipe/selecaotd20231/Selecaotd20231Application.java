d2e103a48cd1abec15d9e5d64e3bd6777eb8d501
TheKoala <felipelima8556@gmail.com>
2023-03-04 09:31:40 -0300

    first commit

---
+++ src/main/java/lima/felipe/selecaotd20231/Selecaotd20231Application.java
@@ -0,0 +1,13 @@
+package lima.felipe.selecaotd20231;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Selecaotd20231Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Selecaotd20231Application.class, args);
+	}
+
+}
---
+++ src/test/java/lima/felipe/selecaotd20231/Selecaotd20231ApplicationTests.java
@@ -0,0 +1,13 @@
+package lima.felipe.selecaotd20231;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Selecaotd20231ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
